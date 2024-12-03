aaaac726671385dd7003c926e6067cc5605679f7
jones-bass <jonesbass.tb@gmail.com>
2024-12-02 21:52:26 -0300

    Add EventScroll

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,14 +2,26 @@
 
 import Link from 'next/link';
 import { BiSearch } from 'react-icons/bi';
+import { useScroll } from '../hooks/useScroll';
+import logo from '../../../public/logo.png'
+import Image from 'next/image';
 
 export default function Header() {
+  const isScrolled = useScroll();
+
   return (
     <header
-      className='fixed top-0 z-50 flex w-full items-center justify-between bg-gradient-to-t from-transparent to-black p-2 px-4 transition-all lg:px-16 lg:py-4'>
+      className={`${isScrolled && 'bg-black'}
+      fixed top-0 z-50 flex w-full items-center justify-between bg-gradient-to-t from-transparent to-black p-2 px-4 transition-all lg:px-16 lg:py-4`}>
       <div className='flex items-center space-x-2 md:space-x-8'>
         <Link href='/'>
-          Logo downFilmes
+          <Image
+            src={logo}
+            alt='Logo'
+            width={150}
+            height={150}
+            className='cursor-pointer'
+          />
         </Link>
 
         <nav>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
 
       <main className='relative lg:space-y-24'>
         <Banner />
-        <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        <div className="grid grid-cols-1 pl-10 pr-10 gap-6 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
           {movies.map((movie) => (
             <MovieCard key={movie.id} movie={movie} />
           ))}
